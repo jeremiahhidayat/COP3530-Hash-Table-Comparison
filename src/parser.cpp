2d6c8c9023e6440cfd861ec9b3b30abd57118b47
Jeremiah Hidayat <jeremiahhidayat@yahoo.com>
2025-11-02 21:15:33 -0500

    Create Project Structure and Movie Struct (issue #10)

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "parser.h"
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+vector<Movie> parserMovies(const string &filename) {
+    vector<Movie> movies;
+    ifstream file(filename);
+    string line;
+    getline(file, line);
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string token;
+        Movie movie;
+
+        getline(ss, movie.id, '\t'); // tconst
+        std::getline(ss, token, '\t'); // titleType
+        std::getline(ss, movie.title, '\t'); //primaryTitle
+
+        std::getline(ss, token, '\t'); // originalTitle
+        std::getline(ss, token, '\t'); // isAdult
+
+        std::getline(ss, token, '\t'); // startYear
+        movie.year = (token != "\\N") ? std::stoi(token) : -1;
+
+        std::getline(ss, token, '\t'); // endYear (skip)
+        std::getline(ss, token, '\t'); // runtimeMinutes
+
+        std::getline(ss, movie.genres, '\t');
+
+        movies.push_back(movie);
+    }
+
+    return movies;
+}
