3905c64ffa3749238abf23cbc4080e3f988e80d8
DigitalHail <liamsgale@gmail.com>
2025-11-04 01:17:57 -0500

    what a wonderful world

--- include/hashTemplate.h
+++ include/hashTemplate.h
@@ -17,7 +17,7 @@ class hashTable {
 public:
     using keyValuePair = std::pair<keyType, valueType>;
 
-    hashTable(size_t initCapacity = 10, float loadFactor = 0.75f)
+    explicit hashTable(size_t initCapacity = 10, float loadFactor = 0.75f)
     :capacity(initCapacity), loadFactor(loadFactor), elements(0) {}
 
     virtual ~hashTable() = default;
@@ -27,9 +27,9 @@ public:
     virtual valueType search(const keyType& key) const = 0;
 
     //getters
-    float getLoadFactor() const { return static_cast<float>(elements) / capacity; }
-    size_t getCapacity() const { return capacity; }
-    size_t getSize() const { return elements; }
+    [[nodiscard]] float getLoadFactor() const { return static_cast<float>(elements) / capacity; }
+    [[nodiscard]] size_t getCapacity() const { return capacity; }
+    [[nodiscard]] size_t getSize() const { return elements; }
 
     //Print for Debugging
     virtual void print() const = 0;
@@ -47,6 +47,42 @@ protected:
     //needed for open addressing
     virtual void rehash(size_t new_capacity) = 0;
 
+};
+template <typename keyType, typename valueType>
+class hashTableSC : public hashTable<keyType, valueType> {
+  private:
+    std::list<valueType>* arr;
+  public:
+    using hashTable<keyType, valueType>::capacity;
+    using hashTable<keyType, valueType>::keyValuePair;
+    explicit hashTableSC()
+       : hashTable<keyType, valueType>()  // ✅ initialize base class
+   {
+        arr = new std::list<typename hashTable<keyType, valueType>::keyValuePair>[capacity];
+   }
+    ~hashTableSC() override {
+        delete[] arr;
+    }
+    void insert(const keyType& key, const valueType& value) override {
+        arr[hash(key)].push_back(std::make_pair(key, value));
+    }
+    bool remove(const keyType& key) override {
+        auto before = arr[hash(key)].size();
+        arr[hash(key)].remove_if([key](const std::pair<keyType, valueType>& p) {
+        return p.first == key;
+        });
+        auto after = arr[hash(key)].size();
+        if (after < before)
+            return true;
+        return false;
+    }
+    valueType search(const keyType& key) const override {
+        auto it = std::find_if(arr[hash(key)].begin(), arr[hash(key)].end(),
+                           [key](const std::pair<int, std::string>& p) {
+                               return p.first == key;
+                           });
+        return it->second;
+    }
 };
 
 #endif //PROJECT_2_HASHTEMPLATE_H
