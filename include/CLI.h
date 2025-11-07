6cd07913f25efa09c61bba9fc2d926929d80d110
jeremiahhidayat <Jeremiahhidayat@yahoo.com>
2025-11-07 09:25:42 -0500

    Start new CLI for main interface

---
+++ include/CLI.h
@@ -0,0 +1,16 @@
+//
+// Created by jerem on 11/5/2025.
+//
+
+#ifndef CLI_H
+#define CLI_H
+
+
+
+class CLI {
+
+};
+
+
+
+#endif //CLI_H
---
+++ src/CLI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jerem on 11/5/2025.
+//
+
+#include "../include/CLI.h"
